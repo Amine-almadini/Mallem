f483384a4c8de0cb6529f904fdd8e907048e5339
Amine-almadini <amiemadini20@gmail.com>
2026-07-24 10:33:16 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mallem-offline-v5";
+const CACHE_NAME = "mallem-offline-v6";
 const STATIC_CACHE = ["./index.html", "./icon.png"];
 
 self.addEventListener("install", (event) => {
